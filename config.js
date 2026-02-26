7a3d85917d6840d09489d1524d72430ad073dbc3
Jb papa 71 <jbrk9088@gmail.com>
2026-02-26 17:47:59 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    BOT_NAME: "JB Ki Queen Anindita Mini Bot",
+    OWNER_NUMBER: "YOUR_PHONE_NUMBER",  // ex: 919876543210
+    WELCOME_MESSAGE: "Welcome @user to the group!",
+    BOT_IMAGE: "./media/bot-image.jpg",
+    HTML_PAGE: "./public/index.html",
+    PREFIX: "/"
+};

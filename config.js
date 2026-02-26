500990f85cc8e7446721ff1dfe84261b15133c08
Jb papa 71 <jbrk9088@gmail.com>
2026-02-26 19:11:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
+/**
+ * MIT License
+ * JB Ki Queen Anindita Mini Bot
+ * Author: JBPAPA71
+ * License: https://opensource.org/licenses/MIT
+ */
+
 module.exports = {
     BOT_NAME: "JB Ki Queen Anindita Mini Bot",
     OWNER_NUMBER: "YOUR_PHONE_NUMBER",
     WELCOME_MESSAGE: "Welcome @user to the group!",
     BOT_IMAGE: "./media/bot-image.jpg",
-    MENU_IMAGE: "https://files.catbox.moe/427sq3.jpg", // user provided link
+    MENU_IMAGE: "https://files.catbox.moe/427sq3.jpg",
     HTML_PAGE: "./public/index.html",
     PREFIX: "/",
-    PAIR_CODE: "jbpapa71" // pair system code
+    PAIR_CODE: "jbpapa71"
 };
